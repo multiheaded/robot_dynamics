27119308d6de821c7d06e97e1114ca7c7c312e78
Marc Priggemeyer <marc.priggemeyer@rwth-aachen.de>
2022-01-21 13:34:30 +0100

    make Simplifier passable as argument

--- main.py
+++ main.py
@@ -1,186 +1,134 @@
-import numpy as np
-import time
-from sympy import Symbol, Function, pretty, latex, diff, Eq, dotprint, Add, simplify, trigsimp, symbols, shape, pi, expand
-from sympy.abc import theta, alpha, d, a, t, g
-from sympy.core.function import Subs
-from sympy.matrices import Matrix, BlockMatrix
-from sympy.solvers.solveset import linear_eq_to_matrix
+import time, sys, getopt
+from sympy import Symbol, latex, pi
+from sympy.abc import g
+from sympy.matrices import Matrix
 
 from kinematics import SymbolicJointValues, Kinematics
-from dynamics import CylindricBody, Dynamics, Body
+from dynamics import Dynamics, Body
 from simplifier import Simplifier
-from transforms import DHSet, Jacobian
-
-#, eye, zeros, ones, diag, GramSchmidt
-#from sympy.solvers import solve
+from transforms import DHSet
 
 from sympy.interactive.printing import init_printing
 init_printing(use_unicode=True, wrap_line=False, pretty_print=True, latex_mode=True)
 
-
-#first = DHSet(theta = 0, d=d, a=a, alpha=alpha)
-#DHMat = transforms.DH(theta, d, a, alpha)
-#DHMat = DHMat.subs(theta,first.theta)
-
-""" q = SymbolicJointValues(3)
-h1 = 0.5
-h2 = 0.2
-h3 = 0.5
-l  = 0.2
-DHParameters = [  DHSet(q[0]+pi/2, h1, 0, pi/2),
-                  DHSet(q[1], l, 0, -pi/2),
-                  DHSet(-pi/2, q[2]+h2+h3, 0, 0) ]
-STANF = KinematicChain(DHParameters, q)
-print(latex(STANF.forwardKinematics()))
-print(latex(STANF.SymbolicJacobian)) """
-
-"""q = SymbolicJointValues(2)
-l1 = Symbol('l1', real=True)
-m1 = Symbol('m1', real=True)
-r1 = Symbol('r1', real=True)
-l2 = Symbol('l2', real=True)
-m2 = Symbol('m2', real=True)
-r2 = Symbol('r2', real=True)
-DHParameters = [  DHSet(q[0], 0, l1, 0),
-                  DHSet(q[1], 0, l2, 0) ]
-
-tic = time.perf_counter()
-SCARA = Kinematics(DHParameters, q)
-toc = time.perf_counter()
-print(f"Kinematics done. Took {toc - tic:0.4f} seconds")
-print(SCARA.forward(1,2))
-
-#Bodies = [  Body(CoM=Matrix([-l1/2, 0, 0])),
-#            Body(CoM=Matrix([-l2/2, 0, 0])) ]
-Bodies = [ CylindricBody(m1, l1, r1), CylindricBody(m2, l2, r2) ]
-Gravitation = Matrix([0,g,0])
-
-tic = time.perf_counter()
-DynamicSCARA = Dynamics(SCARA, Bodies, Gravitation)
-toc = time.perf_counter()
-print(f"Dynamics done. Took {toc - tic:0.4f} seconds")
-
-tic = time.perf_counter()
-T_inv = simplify(DynamicSCARA.Theta.inv())
-toc = time.perf_counter()
-print(f"Forward dynamics done. Took {toc - tic:0.4f} seconds")
-
-tic = time.perf_counter()
-L = simplify(DynamicSCARA.Theta.cholesky(False))
-toc = time.perf_counter()
-print(f"Decomposition done. Took {toc - tic:0.4f} seconds")
-
-tic = time.perf_counter()
-testmat = simplify(L*L.transpose())
-toc = time.perf_counter()
-print(f"LL* done. Took {toc - tic:0.4f} seconds")
-
-print(latex(simplify(DynamicSCARA.Theta)))
-
-print("")
-
-print(latex(simplify(trigsimp(expand(testmat[1,1])))))
-
-
-#print(latex(SCARA.forwardKinematics()))
-#print(latex(SCARA.SymbolicJacobian)) """
-
 def SymbolicConstantVector(name, N):
   return Matrix([Symbol('%s%d'%(name, i+1), real=True) for i in range(N)])
 
-q = SymbolicJointValues(6)
-a2 = Symbol('a2', real=True)
-a3 = Symbol('a3', real=True)
-d1 = Symbol('d1', real=True)
-d4 = Symbol('d4', real=True)
-d5 = Symbol('d5', real=True)
-d6 = Symbol('d6', real=True)
+def printHelp():
+    print('main.py -s <simplifier>')
+    print(' simplifier is \"trigsimp\", \"simplify\", or \"None\"')
 
-m = SymbolicConstantVector("m", 6)
-x = SymbolicConstantVector("x", 6)
-y = SymbolicConstantVector("y", 6)
-z = SymbolicConstantVector("z", 6)
+def main(argv):
+    q = SymbolicJointValues(6)
+    a2 = Symbol('a2', real=True)
+    a3 = Symbol('a3', real=True)
+    d1 = Symbol('d1', real=True)
+    d4 = Symbol('d4', real=True)
+    d5 = Symbol('d5', real=True)
+    d6 = Symbol('d6', real=True)
 
-simp = Simplifier("None")
+    m = SymbolicConstantVector("m", 6)
+    x = SymbolicConstantVector("x", 6)
+    y = SymbolicConstantVector("y", 6)
+    z = SymbolicConstantVector("z", 6)
 
-# DH Parameters from https://www.universal-robots.com/articles/ur/application-installation/dh-parameters-for-calculations-of-kinematics-and-dynamics/
-#                       theta,  d,  a,   alpha
-DHParameters = [  DHSet( q[0], d1,  0,  pi/2.0),
-                  DHSet( q[1],  0, a2,       0),
-                  DHSet( q[2],  0, a3,       0),
-                  DHSet( q[3], d4,  0,  pi/2.0),
-                  DHSet( q[4], d5,  0, -pi/2.0),
-                  DHSet( q[5], d6,  0,       0) ]
+    simp = Simplifier("None")
 
-print("Computing kinematics")
-tic = time.perf_counter()
-SCARA = Kinematics(DHParameters, q)
-toc = time.perf_counter()
-print(f"Kinematics done. Took {toc - tic:0.4f} seconds")
+    try:
+        opts, args = getopt.getopt(argv,"hs:",["simplifier="])
+    except getopt.GetoptError:
+        printHelp()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('main.py -s <simplifier>')
+            print(' simplifier is \"trigsimp\", \"simplify\", or \"None\"')
+            sys.exit()
+        elif opt in ("-s", "--simplifier"):
+            simp = Simplifier(arg)
 
-Bodies = [ Body(CoM=Matrix([x[i], y[i], z[i]]), Mass=m[i]) for i in range(6)]
-Gravitation = Matrix([0,g,0])
+    print(f"Using \"%s\" simplifier"%simp.name)    
 
-tic = time.perf_counter()
-DynamicSCARA = Dynamics(SCARA, Bodies, Gravitation)
-toc = time.perf_counter()
-print(f"Dynamics done. Took {toc - tic:0.4f} seconds")
+    # DH Parameters from https://www.universal-robots.com/articles/ur/application-installation/dh-parameters-for-calculations-of-kinematics-and-dynamics/
+    #                       theta,  d,  a,   alpha
+    DHParameters = [  DHSet( q[0], d1,  0,  pi/2.0),
+                    DHSet( q[1],  0, a2,       0),
+                    DHSet( q[2],  0, a3,       0),
+                    DHSet( q[3], d4,  0,  pi/2.0),
+                    DHSet( q[4], d5,  0, -pi/2.0),
+                    DHSet( q[5], d6,  0,       0) ]
 
-print("Inverting Theta")
-tic = time.perf_counter()
-T_inv = simplify(DynamicSCARA.Theta.inv())
-toc = time.perf_counter()
-print(f"Forward dynamics done. Took {toc - tic:0.4f} seconds")
+    print("Computing kinematics")
+    tic = time.perf_counter()
+    SCARA = Kinematics(DHParameters, q, simp)
+    toc = time.perf_counter()
+    print(f"Kinematics done. Took {toc - tic:0.4f} seconds")
 
-tic = time.perf_counter()
-L = simplify(DynamicSCARA.Theta.cholesky(False))
-toc = time.perf_counter()
-print(f"Decomposition done. Took {toc - tic:0.4f} seconds")
+    Bodies = [ Body(CoM=Matrix([x[i], y[i], z[i]]), Mass=m[i]) for i in range(6)]
+    Gravitation = Matrix([0,g,0])
 
-tic = time.perf_counter()
-testmat = simplify(L*L.transpose())
-toc = time.perf_counter()
-print(f"LL* done. Took {toc - tic:0.4f} seconds")
+    tic = time.perf_counter()
+    DynamicSCARA = Dynamics(SCARA, Bodies, Gravitation, simp)
+    toc = time.perf_counter()
+    print(f"Dynamics done. Took {toc - tic:0.4f} seconds")
 
-print("")
-print("")
-print("")
-print("")
-print("T")
-print(latex(DynamicSCARA.Theta))
+    print("Inverting Theta")
+    tic = time.perf_counter()
+    T_inv = simp.execute(DynamicSCARA.Theta.inv())
+    toc = time.perf_counter()
+    print(f"Forward dynamics done. Took {toc - tic:0.4f} seconds")
 
-print("")
-print("")
-print("")
-print("")
-print("CORZEN")
-print(latex(DynamicSCARA.CORZEN))
+    tic = time.perf_counter()
+    L = simp.execute(DynamicSCARA.Theta.cholesky(False))
+    toc = time.perf_counter()
+    print(f"Decomposition done. Took {toc - tic:0.4f} seconds")
 
-print("")
-print("")
-print("")
-print("")
-print("GRAV")
-print(latex(DynamicSCARA.Grav))
+    tic = time.perf_counter()
+    testmat = simp.execute(L*L.transpose())
+    toc = time.perf_counter()
+    print(f"LL* done. Took {toc - tic:0.4f} seconds")
 
-print("")
-print("")
-print("")
-print("")
-print("T_inv")
-print(latex(T_inv))
+    print("")
+    print("")
+    print("")
+    print("")
+    print("T")
+    print(latex(DynamicSCARA.Theta))
 
-print("")
-print("")
-print("")
-print("")
-print("L")
-print(latex(L))
+    print("")
+    print("")
+    print("")
+    print("")
+    print("CORZEN")
+    print(latex(DynamicSCARA.CORZEN))
 
-print("")
-print("")
-print("")
-print("")
-print("testmat (should be equal to T)")
-print(latex(testmat))
+    print("")
+    print("")
+    print("")
+    print("")
+    print("GRAV")
+    print(latex(DynamicSCARA.Grav))
 
+    print("")
+    print("")
+    print("")
+    print("")
+    print("T_inv")
+    print(latex(T_inv))
+
+    print("")
+    print("")
+    print("")
+    print("")
+    print("L")
+    print(latex(L))
+
+    print("")
+    print("")
+    print("")
+    print("")
+    print("testmat (should be equal to T)")
+    print(latex(testmat))
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
--- simplifier.py
+++ simplifier.py
@@ -1,12 +1,16 @@
+from unicodedata import name
 from sympy.simplify import simplify, trigsimp
 
 class Simplifier:
     execute = None
+    name = None
     
     def __init__(self, name = "simplify"):
+        self.name = name
         if name == "simplify":
             self.execute = simplify
         elif name == "trigsimp":
             self.execute = trigsimp
         else:
+            name = "None"
             self.execute = lambda x : x
